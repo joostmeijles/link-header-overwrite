90bb1ed52a1f402620f674aad3fafcfba3de99ae
Joost Meijles <joost.meijles@avivasolutions.nl>
2024-09-05 08:56:03 +0200

    Add preconnect Link header

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: async () => {
+    return [
+      // Add Link header with preconnect for all pages
+      // This results in faster retrieval of the first image by the browser
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "header",
+            key: "Sec-Fetch-Dest",
+            value: "document", // Match only on page/document requests
+          },
+        ],
+        headers: [
+          {
+            key: "Link",
+            value: "<https://example.com>; rel=preconnect",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
